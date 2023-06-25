d75fdfcbfe6e757e8541fb84186dceb9b852e242
Toru Choi <blueocean239@gmail.com>
2023-06-25 16:47:26 +0900

    added domain module with dependencies

--- domain/src/main/java/kr/toru/purekotlindomain/domain/Domain.kt
+++ domain/src/main/java/kr/toru/purekotlindomain/domain/Domain.kt
@@ -1,4 +1,12 @@
 package kr.toru.purekotlindomain.domain
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
 class Domain {
+    fun test() = "TEST!!"
+
+    suspend fun test1() = withContext(Dispatchers.IO) {
+
+    }
 }
