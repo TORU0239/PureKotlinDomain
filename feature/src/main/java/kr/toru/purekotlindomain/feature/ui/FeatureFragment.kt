77f5ad638f2867a17859c99afd278b93157cd3a0
Toru Choi <blueocean239@gmail.com>
2023-06-25 23:47:19 +0900

    making it like common app

--- feature/src/main/java/kr/toru/purekotlindomain/feature/ui/FeatureFragment.kt
+++ feature/src/main/java/kr/toru/purekotlindomain/feature/ui/FeatureFragment.kt
@@ -1,15 +1,25 @@
 package kr.toru.purekotlindomain.feature.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 import kr.toru.purekotlindomain.feature.R
 import kr.toru.purekotlindomain.feature.viewmodel.FeatureViewModel
 
 class FeatureFragment : Fragment() {
+
+    private val viewModel: FeatureViewModel by lazy {
+        ViewModelProvider(this@FeatureFragment).get(FeatureViewModel::class.java)
+    }
+
+    private lateinit var textView: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,6 +27,16 @@ class FeatureFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_feature, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        textView = view.findViewById(R.id.tvResult)
+        viewLifecycleOwner.lifecycleScope.launch {
+            val result = viewModel.getInfo("Toru")
+            Log.e("Toru", "age: ${result.age} | name: ${result.name} | count: ${result.count}")
+            textView.text = "age: ${result.age} | name: ${result.name} | count: ${result.count}"
+        }
     }
 
     companion object {
--- feature/src/main/java/kr/toru/purekotlindomain/feature/viewmodel/FeatureViewModel.kt
+++ feature/src/main/java/kr/toru/purekotlindomain/feature/viewmodel/FeatureViewModel.kt
@@ -1,6 +1,14 @@
 package kr.toru.purekotlindomain.feature.viewmodel
 
 import androidx.lifecycle.ViewModel
+import kr.toru.purekotlindomain.data.ExampleResponse
+import kr.toru.purekotlindomain.usecase.ExampleUseCase
+import kr.toru.purekotlindomain.usecase.impl.ExampleUseCaseImpl
 
 class FeatureViewModel: ViewModel() {
+    private val useCase: ExampleUseCase = ExampleUseCaseImpl()
+
+    suspend fun getInfo(name: String): ExampleResponse {
+        return useCase.getInfo(name)
+    }
 }
