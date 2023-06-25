1b08face4ea1eb05f463359fcf967150c4a439e2
Toru Choi <blueocean239@gmail.com>
2023-06-25 18:19:55 +0900

    ktor get request and response example

--- app/src/main/java/kr/toru/purekotlindomain/MainActivity.kt
+++ app/src/main/java/kr/toru/purekotlindomain/MainActivity.kt
@@ -1,11 +1,27 @@
 package kr.toru.purekotlindomain
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kr.toru.purekotlindomain.domain.HttpClient
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        test()
+    }
+
+    private fun test() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val result = HttpClient.getTest(name = "Toru")
+            CoroutineScope(Dispatchers.Main).launch {
+                findViewById<TextView>(R.id.text).text = "${result.name}, ${result.count}, ${result.age}"
+            }
+        }
     }
 }
--- domain/src/main/java/kr/toru/purekotlindomain/domain/Domain.kt
+++ domain/src/main/java/kr/toru/purekotlindomain/domain/Domain.kt
@@ -1,12 +1,84 @@
 package kr.toru.purekotlindomain.domain
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.DefaultRequest
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.plugins.observer.ResponseObserver
+import io.ktor.client.request.get
+import io.ktor.client.request.header
+import io.ktor.client.request.parameter
+import io.ktor.client.request.url
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
+import io.ktor.http.parametersOf
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
 
-class Domain {
-    fun test() = "TEST!!"
+private const val TIMEOUT = 6000
+private const val URL = "https://api.agify.io/"
 
-    suspend fun test1() = withContext(Dispatchers.IO) {
+object HttpClient {
+    private val httpClientAndroid = HttpClient(Android) {
 
+        install(ContentNegotiation) {
+            json(
+                Json {
+                    prettyPrint = true
+                    isLenient = true
+                    ignoreUnknownKeys = true
+                }
+            )
+
+
+            engine {
+                connectTimeout = TIMEOUT
+                socketTimeout = TIMEOUT
+            }
+        }
+
+        install(Logging) {
+            logger = object : Logger {
+                override fun log(message: String) {
+                    println("Logger Ktor => $message")
+                }
+
+            }
+            level = LogLevel.BODY
+        }
+
+        install(ResponseObserver) {
+            onResponse { response ->
+                println("HTTP status: ${response.status.value}")
+            }
+        }
+
+        install(DefaultRequest) {
+            header(
+                key = HttpHeaders.ContentType,
+                value = ContentType.Application.Json
+            )
+        }
+    }
+
+    suspend fun getTest(name: String): Response {
+        return kotlin.runCatching {
+            httpClientAndroid.get {
+                url(urlString = URL)
+                parameter(key = "name", value = name)
+            }.body<Response>()
+        }.getOrDefault(Response())
     }
 }
+
+@Serializable
+data class Response(
+    val count: Int = -1,
+    val name: String = "empty",
+    val age: Int = -1,
+)
